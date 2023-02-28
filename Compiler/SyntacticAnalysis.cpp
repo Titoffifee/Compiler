465bd4577f22b0069801cb3fb3b391f590530362
Titov <Titovfedor22@yandex.ru>
2023-03-01 01:31:20 +0300

    Написал синтаксический анализатор для if, for, while, создания переменных

--- Compiler/SyntacticAnalysis.cpp
+++ Compiler/SyntacticAnalysis.cpp
@@ -24,10 +24,150 @@ void BaseType(std::vector<Lexeme>& lexemes, int& i) {
     throw; // Ожидался тип
 }
 
+void Expression(std::vector<Lexeme>& lexemes, int& i);
+
+void Equal(std::vector<Lexeme>& lexemes, int& i);
+
+void VariableInit(std::vector<Lexeme>& lexemes, int& i) {
+    if (lexemes[i] != Type::Ident) 
+        throw; // Ожидалось имя
+    ++i;
+    if (lexemes[i] == Type::Equal) {
+        ++i;
+        Expression(lexemes, i);
+    }
+}
+
+void NewVariable(std::vector<Lexeme>& lexemes, int& i) {
+    BaseType(lexemes, i);
+    VariableInit(lexemes, i);
+    while (lexemes[i] == Type::Comma) {
+        ++i;
+        VariableInit(lexemes, i);
+    }
+}
+
+void If(std::vector<Lexeme>& lexemes, int& i) {
+    if (lexemes[i].value_ != "if")
+        throw; // Ожидалось слово "if"
+    ++i;
+    if (lexemes[i] != Type::LeftRoundBracket)
+        throw; // Ожидалась открывающая круглая 
+    ++i;
+    Expression(lexemes, i);
+    if (lexemes[i] != Type::RightRoundBracket)
+        throw; // Ожидалась закрывающая круглая 
+    ++i;
+    if (lexemes[i] != Type::LeftBrace)
+        throw; // Ожидалась открывающая фигурная 
+    ++i;
+    Block(lexemes, i);
+    if (lexemes[i] != Type::RightBrace)
+        throw; // Ожидалась закрывающая фигурная
+    ++i;
+    if (lexemes[i] == Type::Special && lexemes[i].value_ == "else") {
+        ++i;
+        if (lexemes[i] != Type::LeftBrace)
+            throw; // Ожидалась открывающая фигурная 
+        ++i;
+        Block(lexemes, i);
+        if (lexemes[i] != Type::RightBrace)
+            throw; // Ожидалась закрывающая фигурная
+        ++i;
+    }
+}
+
+void While(std::vector<Lexeme>& lexemes, int& i) {
+    if (lexemes[i].value_ != "while")
+        throw; // Ожидалось слово "while"
+    ++i;
+    if (lexemes[i] != Type::LeftRoundBracket)
+        throw; // Ожидалась открывающая круглая 
+    ++i;
+    Expression(lexemes, i);
+    if (lexemes[i] != Type::RightRoundBracket)
+        throw; // Ожидалась закрывающая круглая 
+    ++i;
+    if (lexemes[i] != Type::LeftBrace)
+        throw; // Ожидалась открывающая фигурная 
+    ++i;
+    Block(lexemes, i);
+    if (lexemes[i] != Type::RightBrace)
+        throw; // Ожидалась закрывающая фигурная
+    ++i;
+}
+
+void ForVariableInit(std::vector<Lexeme>& lexemes, int& i) {
+    NewVariable(lexemes, i);
+    while (lexemes[i] == Type::Dot) {
+        ++i;
+        NewVariable(lexemes, i);
+    }
+}
+
+void For(std::vector<Lexeme>& lexemes, int& i) {
+    if (lexemes[i].value_ != "for")
+        throw; // Ожидалось слово "for"
+    ++i;
+    if (lexemes[i] != Type::LeftRoundBracket)
+        throw; // Ожидалась открывающая круглая 
+    ++i;
+
+    if (lexemes[i] != Type::Semicolon) {
+        ForVariableInit(lexemes, i);
+    }
+    if (lexemes[i] != Type::Semicolon)
+        throw; // Нужна точка с запятой
+    ++i;
+
+    Expression(lexemes, i);
+    if (lexemes[i] != Type::Semicolon)
+        throw; // Нужна точка с запятой
+    ++i;
+
+    if (lexemes[i] != Type::RightRoundBracket) {
+        Equal(lexemes, i);
+        while (lexemes[i] == Type::Semicolon) {
+            ++i;
+            Equal(lexemes, i);
+        }
+    }
+
+    if (lexemes[i] != Type::RightRoundBracket)
+        throw; // Ожидалась закрывающая круглая
+    ++i;
+
+    if (lexemes[i] != Type::LeftBrace)
+        throw; // Ожидалась открывающая фигурная 
+    ++i;
+    Block(lexemes, i);
+    if (lexemes[i] != Type::RightBrace)
+        throw; // Ожидалась закрывающая фигурная
+    ++i;
+}
+
 void Action(std::vector<Lexeme>& lexemes, int& i) {
     if (lexemes[i] == Type::Special) {
+        if (lexemes[i].value_ == "if") {
+            If(lexemes, i);
+            return;
+        }
+        if (lexemes[i].value_ == "while") {
+            While(lexemes, i);
+            return;
+        }
+        if (lexemes[i].value_ == "for") {
+            For(lexemes, i);
+            return;
+        }
+        throw; //Неправильное спец слово
+    }
+    
+    if (lexemes[i] == Type::Ident) {
 
     }
+
+    NewVariable(lexemes, i);
 }
 
 void Block(std::vector<Lexeme>& lexemes, int& i) {
